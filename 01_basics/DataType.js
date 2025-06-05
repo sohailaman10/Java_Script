2edd4e2578ede59432f4e01521572a4fcabd120e
sohailaman10 <shaiksohailaman4@gmail.com>
2025-06-05 06:11:33 +0000

    Added DataType and Variables example

---
+++ 01_basics/DataType.js
@@ -0,0 +1,15 @@
+// alert(3 + 3) We are using nodejs, not chrome
+
+let name = "sohail aman"
+let age = 20
+let islogIn = false
+let state = null
+
+// number => 2 to power 53
+// String => ""
+// boolean => true/false
+// NULL => standalone value
+// undefined => not defined value
+// Symbol => using for unqiue one
+
+// Object 
---
+++ 01_basics/Variables.js
@@ -0,0 +1,15 @@
+const accId = 132
+let accEmail = "soahiosnak@gmial.com"
+var accPassword = "2345678"
+accity = "Tirupati" 
+
+// I want change all detail to test the which is allowed to change or not
+
+// accId = 2 // is not allowed
+
+// this is allowed to excuted
+accEmail = "sohailaman4@gmail.com"
+accPassword = "21462387"
+accity = "Renigunta"
+
+console.table([accId, accEmail, accPassword, accity])
