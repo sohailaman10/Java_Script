dd02c9e0158aa68cacf0e24869086fde3402120c
Sohail Aman <shaiksohailaman4@example.com>
2025-06-06 12:25:36 +0530

    Added

--- 01_basics/Comparison.js
+++ 01_basics/Comparison.js
@@ -1,1 +1,27 @@
-console.log("sohail aman");
+// *********************** Operation ***************************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+//Arthimatic operator
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 / 2);
+console.log(2 * 2);
+console.log(2 % 2);
+console.log(2 ** 2 + "2+2");
+
+//Concatetion string
+let firstname = " Shazeel"
+let middlename = " Ayaan"
+let lastname = " Shaik"
+
+console.log("MY name = " + firstname + middlename + lastname);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
